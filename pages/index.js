2b26d7025d8b950a3f9c1a8abe7f91cd8b160f2c
чингерс <4ingers@gmail.com>
2020-07-28 16:49:05 +0400

    base installations

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+
+const Index = () => {
+  return (
+    <>
+      <Head><title>Show&News</title></Head>
+    </>
+  )
+}
+
+export default Index
